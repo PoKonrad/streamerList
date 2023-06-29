d960419cda9e8742951a844d3079b78536591272
Konrad Porebski <110181064+PoKonrad@users.noreply.github.com>
2023-06-29 15:19:35 +0200

    style: improved spotlight style

--- frontend/src/components/Spotlight.tsx
+++ frontend/src/components/Spotlight.tsx
@@ -1,4 +1,6 @@
-import { Container, Typography, styled } from "@mui/material";
+import { Paper, Typography, styled } from "@mui/material";
+import { Streamer, StreamerResp } from "../types";
+import React, { useEffect, useState } from "react";
 
 const Background = styled("div")({
   position: "relative",
@@ -16,7 +18,10 @@ const Background = styled("div")({
     position: "absolute",
     zIndex: -1,
     inset: 0,
-    transform: "skewY(-5deg)",
+    transform: "skewY(-5deg) translate(-20px)",
+    margin: 0,
+    padding: 0,
+    width: "102vw",
   },
 });
 
@@ -27,21 +32,63 @@ const SpotlightContainer = styled("div")({
   justifyContent: "center",
 });
 
-const Spotlight = () => {
+const TextContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  transform: "skewY(-4deg)",
+});
+
+const ImageContainer = styled(Paper)({
+  maxWidth: "25rem",
+  maxHeight: "25rem",
+  minWidth: "20rem",
+  minHeight: "20rem",
+  overflow: "hidden",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "2rem",
+  borderRadius: "1rem",
+  transform: "skewY(-4deg)",
+});
+
+interface SpotlightProps {
+  streamers: StreamerResp;
+}
+
+const Spotlight: React.FC<SpotlightProps> = ({ streamers }) => {
+  const [spotlightStreamer, setSpotlightStreamer] = useState<
+    Streamer | undefined
+  >();
+
+  useEffect(() => {
+    setSpotlightStreamer(
+      streamers[Math.floor(Math.random() * streamers?.length)]
+    );
+  }, []);
+
+  if (!streamers) {
+    return null;
+  }
+
   return (
     <SpotlightContainer>
       <Background>
-        <Typography
-          variant="h2"
-          sx={{
-            letterSpacing: "-2px",
-          }}
-        >
-          Today's Spotlight
-        </Typography>
-        <div>
-          <img src="https://cataas.com/cat" width={512} />
-        </div>
+        <TextContainer>
+          <Typography
+            variant="h2"
+            sx={{
+              letterSpacing: "-2px",
+            }}
+          >
+            Today's Spotlight
+          </Typography>
+          <Typography variant="h3">{spotlightStreamer?.name}</Typography>
+        </TextContainer>
+        <ImageContainer>
+          <img src="https://cataas.com/cat" />
+        </ImageContainer>
       </Background>
     </SpotlightContainer>
   );
